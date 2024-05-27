80e44f3fc61e61fd0fe3fd88a7f0871e360e04e5
lomiland <161277582+lomiland@users.noreply.github.com>
2024-05-27 21:04:34 +0800

    Update surakarta_agent_mine.h

--- ClientDemo/src/surakarta_agent_mine.h
+++ ClientDemo/src/surakarta_agent_mine.h
@@ -5,9 +5,4 @@ class SurakartaAgentMine : public SurakartaAgentBase {
 public:
     using SurakartaAgentBase::SurakartaAgentBase;
     SurakartaMove CalculateMove() override;
-    SurakartaAgentMine(std::shared_ptr<SurakartaBoard> board,
-                       std::shared_ptr<SurakartaGameInfo> game_info,
-                       std::shared_ptr<SurakartaRuleManager> rule_manager)
-        : SurakartaAgentBase(board, game_info, rule_manager) {
-    }
 };
